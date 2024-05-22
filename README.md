a2e8e35ea93be4dd911d40fb478db76e3b5dec84
Pawan Yadav <pawanyadavsultania@gmail.com>
2024-05-22 20:16:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# POC-GO-OAUTH2
+
+A POC Project on How to implement OAuth2 in GO
---
+++ cmd/poc-go-oauth2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/sultania23/poc-go-oauth2/internal/configs"
+	"github.com/sultania23/poc-go-oauth2/internal/logger"
+	"github.com/sultania23/poc-go-oauth2/internal/services"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Initialize Viper across the application
+	configs.InitializeViper()
+
+	// Initialize Logger across the application
+	logger.InitializeZapCustomLogger()
+
+	// Initialize Oauth2 Services
+	services.InitializeOAuthFacebook()
+	services.InitializeOAuthGoogle()
+
+	// Routes for the application
+	http.HandleFunc("/", services.HandleMain)
+	http.HandleFunc("/login-fb", services.HandleFacebookLogin)
+	http.HandleFunc("/callback-fb", services.CallBackFromFacebook)
+	http.HandleFunc("/login-gl", services.HandleGoogleLogin)
+	http.HandleFunc("/callback-gl", services.CallBackFromGoogle)
+
+	logger.Log.Info("Started running on http://localhost:" + viper.GetString("port"))
+	log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+}
